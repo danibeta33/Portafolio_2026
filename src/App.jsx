cca19acf94d7d0c43c8336c6e8dc902342f30491
DaniBeta_33 <daniel.betancourtb@upb.edu.co>
2026-03-29 18:17:36 -0500

    Plantilla subida

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Proyecto reiniciado</h1>
+      <p>Base limpia lista para construir desde cero.</p>
+      
+    </main>
+  )
+}
+
+export default App
